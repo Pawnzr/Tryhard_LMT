a53124cce331f7bb2b62f612bb2f2467dc02f414
anduu1506 <anduu150620@gmail.com>
2021-04-08 18:05:59 +0700

    Trac nghiem random

--- src/D1/B1/Server.java
+++ src/D1/B1/Server.java
@@ -8,17 +8,7 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(1506);//1.tạo lớp ServerSocket với port 1506
-        Socket my_client = server.accept();//2. accept kết nối
-
-        DataOutputStream dos = new DataOutputStream(my_client.getOutputStream());
-        DataInputStream dis = new DataInputStream(my_client.getInputStream());
-        while(true){
-        if (dis.readInt()%2==0){
-            dos.writeUTF("So chan");
-        }
-        else{
-            dos.writeUTF("So le");
-        }
+        ServerSocket server = new ServerSocket(1506);
     }
-}}
+
+}
